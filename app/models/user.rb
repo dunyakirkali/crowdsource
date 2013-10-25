59e209cc54ecad18cdaf31c8dde53aba30b608f0
Dunya Kirkali <dunyakirkali@gmail.com>
2013-10-25 19:03:20 +0200

    Fetch user email from omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
    where(auth.slice(:provider, :uid)).first_or_create do |user|
      user.provider = auth.provider
      user.uid = auth.uid
+     user.email = auth.info.email
      user.username = auth.info.nickname
    end
   end
