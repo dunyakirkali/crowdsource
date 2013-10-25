ea6d95ed5245bd27705753afce19f6b7b5a228a3
Dunya Kirkali <dunyakirkali@gmail.com>
2013-10-25 18:50:07 +0200

    Improved omniauth login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,6 +1,12 @@
 class OmniauthCallbacksController < ApplicationController
   def all
-    raise request.env["omniauth.auth"].to_yaml
+    user = User.from_omniauth(request.env["omniauth.auth"])
+    if user.persisted?
+      sign_in_and_redirect user, notice: "Signed in!"
+    else
+      session["devise.user_attributes"] = user.attributes
+      redirect_to new_user_registration_url
+    end
   end
   alias_method :github, :all
 end
